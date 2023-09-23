eded6edd7e7254f4b48cdf9f11d73672c3bdda54
Rusheel Dasari <rusheel@gmail.com>
2023-09-23 17:33:02 -0500

    mongoDB backend

--- src/clear.py
+++
@@ -1,5 +0,0 @@
-import redis
-
-db_client = redis.Redis(host='localhost', port=6379, db=0)
-for key in db_client.keys():
-    db_client.delete(key)
--- src/main.py
+++ src/main.py
@@ -1,16 +1,40 @@
+import random
+from bson import ObjectId
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import redis
-from redisearch import TextField, TagField, NumericField, Query, IndexDefinition
+from pymongo import MongoClient
 import uvicorn
 import json
 
 
 class ServerData:
-    def __init__(self, app, db_client, db_index):
+    def __init__(self, app, db_client):
         self.app = app
         self.db_client = db_client
-        self.db_index = db_index
+        self.db_table = db_client["news"]["Articles"]
+        self.articles = self.db_table.find()
+
+    def refresh_articles(self):
+        self.articles = self.db_table.find()
+
+    @staticmethod
+    def map_with_body(article: dict):
+        a = {}
+        a["title"] = article["title"]
+        a["date"] = article["date"]
+        a["tags"] = list(
+            map(lambda x: x.strip(), article["hashtags"].strip().split(',')))
+        a["id"] = article["_id"]
+        a["images"] = article["urlToImage"]
+        a["body"] = article["news_article"]
+        a["popularity"] = random.randint(-30, 70)
+        return a
+
+    @staticmethod
+    def map_without_body(article: dict):
+        a = article.copy()
+        a['body'] = None
+        return a
 
 
 app = FastAPI()
@@ -22,24 +46,10 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-db_client = redis.Redis(host='localhost', port=6379, db=0)
+uri = "mongodb+srv://muthu:adi123@cluster0.rgzozbh.mongodb.net/?retryWrites=true&w=majority"
+db_client = MongoClient(uri)
 
-schema = (
-    TagField("$.sources", as_name="sources"),
-    TagField("$.tags", as_name="tags"),
-    TextField("$.id", as_name="id"),
-    TextField("$.title", as_name="title"),
-    TextField("$.image", as_name="image"),
-    TextField("$.date", as_name="date"),
-    TextField("$.body", as_name="body"),
-    NumericField("$.popularity", as_name="popularity"),
-)
-
-db_index = db_client.ft("idx:articles")
-db_index.create_index(schema, definition=IndexDefinition(
-    prefix=["article:"]))
-
-server_data = ServerData(app, db_client, db_index)
+server_data = ServerData(app, db_client)
 
 
 @server_data.on_event("startup")
@@ -59,20 +69,17 @@ def root():
 
 @server_data.app.get('/api/articles')
 def list_articles(page: int = 1):
-    articles = [{} for _ in range(10)]
+    server_data.refresh_articles()
+    articles = server_data.articles[(page-1)*10: 10*page]
+    articles = list(map(server_data.map_without_body, articles))
     return json.dumps(articles)
 
 
 @server_data.app.get('/api/articles/{id}')
-def show_article(id: int):
-    article = server_data.db_index.search(Query(f"@id:{id}")).docs.json
+def show_article(id: str):
+    article = list(map(server_data.map_with_body,
+                       server_data.db_table.find(ObjectId(id))))
     return json.dumps(article)
-
-
-@server_data.app.get('/api/search')
-def search_articles(q: str):
-    articles = []
-    return json.dumps(articles)
 
 
 if __name__ == '__main__':
